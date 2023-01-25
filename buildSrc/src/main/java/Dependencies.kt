a199a1e66df48bf9647b27540944d1132ec9ed7e
daniatitienei <daniatitienei@gmail.com>
2023-01-26 00:10:07 +0200

    converted :app/build.gradle to kts and added dependencies to buildSrc

---
+++ buildSrc/src/main/java/Dependencies.kt
@@ -0,0 +1,10 @@
+object Dependencies {
+    const val coreKtx = "androidx.core:core-ktx:1.9.0"
+    const val lifecycleRuntimeKtx = "androidx.lifecycle:lifecycle-runtime-ktx:2.5.1"
+    const val activityCompose = "androidx.activity:activity-compose:1.6.1"
+    const val composeBom = "androidx.compose:compose-bom:2022.10.00"
+    const val composeUi = "androidx.compose.ui:ui"
+    const val composeUiGraphics = "androidx.compose.ui:ui-graphics"
+    const val composeUiToolingPreview = "androidx.compose.ui:ui-tooling-preview"
+    const val composeMaterial3 = "androidx.compose.material3:material3"
+}
---
+++ buildSrc/src/main/java/Testing.kt
@@ -0,0 +1,8 @@
+object Testing {
+    const val jUnit = "junit:junit:4.13.2"
+    const val jUnitExt = "androidx.test.ext:junit:1.1.5"
+    const val composeBom = "androidx.compose:compose-bom:2022.10.00"
+    const val jUnitComposeUiTest = "androidx.compose.ui:ui-test-junit4"
+    const val composeUiTooling = "androidx.compose.ui:ui-tooling"
+    const val composeUiTestManifest = "androidx.compose.ui:ui-test-manifest"
+}
